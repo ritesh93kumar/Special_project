64f8d019b515b6934d2dd5b36238a643701ebda2
ritesh kumar <ritesh93kumar@gmail.com>
2017-10-27 00:31:28 +0530

    added cards nd animation

--- js/script.js
+++ js/script.js
@@ -1,34 +1,53 @@
-var cardContainer= document.getElementById('card_container').children;
-var cards = document.getElementById('card_container').children;
-console.log(cardContainer);
-console.log(cards[0]);
-console.log(cards.length);
+var cradTransition = (function(){
 
-var count;
-
-init();
-
-function init(){
-    var firstCard = cards[0];
-    firstCard.classList.add('card_current');
-    count = 0;
-}
-
-function animateCard(){
-
-    var currentCard = cards[count];
-    currentCard.classList.remove('card_current');
-    currentCard.classList.add('card-fade');
-
-    count++;
-
-    if(count < cards.length){
-        var nextCard = cards[count];
-        nextCard.classList.add('card_current');
-    }else {
-        count = 0;
-        var currentCard = cards[count];
-        currentCard.classList.add('card_current');
-    }
+    var $cardContainer = $('#card_container');
+    var $cards = $cardContainer.children('div.card');
+    var cardCount = $cards.length,
+    animcursor = 1,
+    current = 0,
+    isAnimating = false;
     
-}
+    console.log($cards);
+
+    function init(){
+        $cards.eq( current ).addClass('card_current');
+        
+        $('#nxtBtn').on( 'click', function() {
+			if( isAnimating ) {
+				return false;
+			}
+			if( animcursor > 2 ) {
+				animcursor = 1;
+			}
+			nextCard( animcursor );
+			++animcursor;
+		} );
+    };
+
+    function nextCard(animation){
+        var $currentCard = $cards.eq( current );
+
+        if( current < cardCount - 1 ) {
+            ++current;
+        }
+        else {
+            current = 0;
+        }
+
+        var $nextCard = $cards.eq( current );
+
+        $currentCard.addClass('animated bounceOutLeft');
+        $currentCard.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            $currentCard.removeClass('animated bounceOutLeft card_current');
+            $nextCard.addClass('animated slideInRight card_current');
+
+            $nextCard.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                $nextCard.removeClass('animated slideInRight');
+            });
+        });
+
+        
+    }
+
+    init();
+})();
